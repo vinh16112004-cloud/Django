ef8a80956c75f6355db9fd859413836eab9c9b67
vinh16112004-cloud <vinh16112004@gmail.com>
2026-02-26 23:47:07 +0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+import uuid
+
+class Question(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    grade = models.IntegerField(default=1)
+
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        return False
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey('Enrollment', on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
